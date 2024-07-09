eac57e441573cdf0daff15f04d6026451f598a48
Hanine <91432774+hanine007@users.noreply.github.com>
2024-07-09 11:41:05 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import 'dotenv/config'
 import OpenAI from "openai";
-const openai = new OpenAI({
+const openai = new OpenAI()
+const resulet =await openai.chat.completions.create({
+    model:'gpt-3.5-turbo',
+    messages:[{
+        role:'system',
+    content:'hi brother how are u'    },
 
-})
+    {
+        role:'user',
+        content:'Hi!'
+    },
+            ]
+}) 
+console.log(result)
